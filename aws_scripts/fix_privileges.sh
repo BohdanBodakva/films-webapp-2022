9860960252ce9ab9b90cb706f21b4ff1d4130195
Bohdan Bodakva <bodakvabohdan44@gmail.com>
2023-10-08 23:23:49 +0300

    2

--- aws_scripts/fix_privileges.sh
+++ aws_scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/films_webapp-0.0.1-SNAPSHOT.jar
+chmod +x /home/ec2-user/server/*.jar
 chmod +x /home/ec2-user/server/server_start.sh
 chmod +x /home/ec2-user/server/server_stop.sh
--- aws_scripts/server_start.sh
+++ aws_scripts/server_start.sh
@@ -2,4 +2,4 @@
 # shellcheck disable=SC2164
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
-    films_webapp-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+    *.jar > /dev/null 2> /dev/null < /dev/null &
